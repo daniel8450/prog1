026a330c314e0e1b187a7cf67528c96a9adcb5c3
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 06:17:41 -0600

    Update pi.hs

--- pi.hs
+++ pi.hs
@@ -12,19 +12,33 @@
 --pie :: [Float]
 --pie = (3 + 4) * n 
 --------------------------------------------------------------
-three :: Float
-three = 3
+--three :: Float
+--three = 3
 
-add :: Float -> Float
-add x = x + three
+--add :: Float -> Float
+--add x = x + three
 
-pie :: [Float]
-pie = [(add 4) / [(n*(n+1)*(n+2)) | n <- [0,2..200]]
+--n :: [Float]
+--n = [(add 4) / [(n*(n+1)*(n+2)) | n <- [0,2..200]]
 
-getPie :: [Float]
-getPie = add 4 / pie !! 100
+--pie :: Float
+--pie = (add 4)
 
+
+  c :: Int
+  c = 1
+  
+  check :: Bool
+  if (c) == 1 
+  then pie = x * (4 / (n * (n+1) * (n+2)))
+  c == 0
+  else pie = (-x) * (4 / (n * (n+1) * (n+2)))
+  c == 1
+  
+  
 main = do
-  let x = getPie
-  print getPie
+  let x = pie
+  print pie
+  
+  
   
