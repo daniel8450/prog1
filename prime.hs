420219423279f5991969e8a77d00f18708a2b1dd
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 14:05:35 -0600

    Update prime.hs

--- prime.hs
+++ prime.hs
@@ -3,7 +3,7 @@ prime = isPrime [2..]
   where isPrime (p:xs) = p : isPrime [x | x <- xs, x `mod` p /= 0]
  
 main = do
-putStrLn "Enter nth term to find:"
+putStrLn "Enter nth term:"
 inputjar <- getLine
 let n = read inputjar :: Int
 print (prime !! n)
