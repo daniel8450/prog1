8a5d3d24a9a210be6ca088d5c86b9c9f99ce0479
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 11:59:47 -0600

    Update sorted.hs

--- sorted.hs
+++ sorted.hs
@@ -1,10 +1,13 @@
---sorted :: [Int] -> Bool
-
 quicksort :: [Int] -> [Int]
 quicksort [] = []
 quicksort (x:xs) = quicksort(filter (<=x) xs) 
                         ++ [x] ++ 
                    quicksort(filter (>x)  xs) 
+
+sorted :: [Int] -> Bool
+sorted n = if (n == quicksort n) then True
+           else False
+
 
 main = do
        let x = [2, 7, 3, 11, 5, 13]
