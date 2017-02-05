0c7e913693a77ef062353d090324fde203901784
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 07:26:26 -0600

    Update prime.hs

--- prime.hs
+++ prime.hs
@@ -1,1 +1,13 @@
+--isPrime :: Int -> Int -> Bool
+--isPrime n 0 = False
+--isPrime n 1 = False
+--isPrime n m = if ((mod n m) /= 0) then True
+              else (isPrime n (m-1))
+      
+--prime : Int -> [Int]
+--prime n | (isPrime
 
+main = do
+       let x = [2..n]
+       print (filer ((mod n m) /= 0) x)
+     
