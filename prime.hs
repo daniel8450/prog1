8bce7bfca5552a18bb4966621b8641daa5a82068
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 08:53:41 -0600

    Update prime.hs

--- prime.hs
+++ prime.hs
@@ -1,12 +1,3 @@
---isPrime :: Int -> Int -> Bool
---isPrime n 0 = False
---isPrime n 1 = False
---isPrime n m = if ((mod n m) /= 0) then True
-             -- else (isPrime n (m-1))
-      
---prime : Int -> [Int]
---prime n | (isPrime
-
 prime :: [Int]
 prime = isPrime [2..]
   where isPrime (p:xs) = p : isPrime [x | x <- xs, x `mod` p /= 0]
