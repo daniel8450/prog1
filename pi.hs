60a56eabdfe18adcaba4b127db2d0f3ad894cf1a
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 02:02:50 -0600

    Update pi.hs

--- pi.hs
+++ pi.hs
@@ -30,7 +30,7 @@
 pie :: Float -> Float
 pie k = ((-1)**k) / ((2*k+2)*(2*k+3)*(2*k+4)) 
 
-pie :: Float -> [Float]
-pie k = ((-1)**k) / ((2*k+2)*(2*k+3)*(2*k+4)) ++ [pie (k)]
+pies :: Float -> [Float]
+pies k = ((-1)**k) / ((2*k+2)*(2*k+3)*(2*k+4)) ++ [pie (k)]
 
-main = do print (pie 100)
+main = do print (pies 100)
