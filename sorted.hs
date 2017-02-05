bc251c208bad033f84dd1667476f6503589d07eb
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 14:19:02 -0600

    Update sorted.hs

--- sorted.hs
+++ sorted.hs
@@ -3,8 +3,8 @@ quicksort [] = []
 quicksort (x:xs) = quicksort(filter (<=x) xs) ++ [x] ++ quicksort(filter (>x)  xs) 
 
 sorted :: [Int] -> Bool
-sorted n = if (n == quicksort n) then True
-           else False
+sorted n = if (n == quicksort n) then True ++ "the list is sorted"
+           else False ++ "the list is NOT sorted"
 
 main = do
 putStrLn "Enter list to check:"
