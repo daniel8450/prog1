dd3eed0e8c277fb9cc6be20d59f076337584eb08
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 12:03:13 -0600

    Update sorted.hs

--- sorted.hs
+++ sorted.hs
@@ -1,14 +1,13 @@
 quicksort :: [Int] -> [Int]
 quicksort [] = []
-quicksort (x:xs) = quicksort(filter (<=x) xs) 
-                        ++ [x] ++ 
-                   quicksort(filter (>x)  xs) 
+quicksort (x:xs) = quicksort(filter (<=x) xs) ++ [x] ++ quicksort(filter (>x)  xs) 
 
 sorted :: [Int] -> Bool
 sorted n = if (n == quicksort n) then True
            else False
 
-
 main = do
-       let x = [2, 7, 3, 11, 5, 13]
-       print (sorted x)
+putStrLn "Enter list to check:"
+inputjar <- getLine
+let n = read inputjar :: [Int]
+print (sorted n)
