ebee4e79ff5434c3d67ae3c7a075d67768498caf
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 13:11:13 -0600

    Update reverse.hs

--- reverse.hs
+++ reverse.hs
@@ -3,12 +3,9 @@ toList n
        | n < 1 = []
        | otherwise = toList (n `div` 10) ++ [n `mod` 10]
 
-reverse :: [Int] -> [Int]
-reverse x = tail x ++ [x]
-
 
 main = do
 putStrLn "Enter int to reverse:"
 inputjar <- getLine
 let n = read inputjar :: Int
-print (Main.reverse (toList n))
+print (reverse (toList n))
