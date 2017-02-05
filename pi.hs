835e1aff4bdc451822b4b5915b94c7482e64f55d
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 00:54:54 -0600

    Update pi.hs

--- pi.hs
+++ pi.hs
@@ -11,16 +11,19 @@
 
 --pie :: Float
 --pie = (3 + 4) * n !! 100
+-------------------------------------------------------------
+--three :: Float
+--three = 3
 
-three :: Float
-three = 3
+--add :: Float -> Float
+--add x = x + three
 
-add :: Float -> Float
-add x = x + three
+--pie :: [Float]
+--pie = [add 4 / (n*(n+1)*(n+2)) | n + 2 <- [0..100]]
 
-pie :: [Float]
-pie = [add 4 / (n*(n+1)*(n+2)) | n + 2 <- [0..100]]
-
+pie :: Float -> Float
+pie = let k = k <- [0..100]
+      in (-1**k / (2*k+2)*(2*k+3)*(2*k+4))
 
 main = do
   let x = pie !! 100
