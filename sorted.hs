5f7700f8658449781becea3804a28bad32398a71
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 09:21:32 -0600

    Update sorted.hs

--- sorted.hs
+++ sorted.hs
@@ -1,11 +1,5 @@
 --sorted :: [Int] -> Bool
 
 main = do
-       let x = [1..10]
-       print (take 5 x)
-       print (drop 5 x)
-       print (head x)
-       print (last x)
-       print (init x)
-       print (tail x)
-       print (x !! 4)
+       print ( [1, 2, 3, 4, 5]  )
+       print ( [2, 3] ++ [3, 4] )
