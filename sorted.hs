dbd1fefb9ec4a871bc73c03a012adc256a4be65f
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 09:18:56 -0600

    Update sorted.hs

--- sorted.hs
+++ sorted.hs
@@ -1,1 +1,11 @@
-sorted :: [Int] -> Bool
+--sorted :: [Int] -> Bool
+
+main = do
+       let x = [1..10]
+       print (take 5 x)
+       print (drop 5 x)
+       print (head x)
+       print (last x)
+       print (init x)
+       print (tail x)
+       print (x !! 4)
