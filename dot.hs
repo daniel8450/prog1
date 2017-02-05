8b211bba0c8b2f0c73117e6ce538646dc3452685
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 14:15:28 -0600

    Update dot.hs

--- dot.hs
+++ dot.hs
@@ -3,6 +3,10 @@ dot [] [] = 0
 dot (x:xs) (y:ys) = x * y + dot xs ys
 
 main = do
-let y = [1,2,3]
-let x = [4,5,6]
-print (dot x y)
+putStrLn "Enter list1:"
+inputjar <- getLine
+let n = read inputjar :: [Int]
+putStrLn "Enter list2:"
+inputjar <- getLine
+let m = read inputjar :: [Int]
+print (dot n m)
