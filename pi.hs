48b054184b50637ea65adb7c1aa75fb5dfffdf7e
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-04 22:40:13 -0600

    Update pi.hs

--- pi.hs
+++ pi.hs
@@ -1,7 +1,7 @@
 n :: [Float]
 n = [-1**k / (2k+2)*(2k+3)*(2k+4) | k <- [0..100]]
-pie :: (Float -> [Float]) -> Float
-pie = (3 + 4) * n
+pie :: Float -> [Float] -> Float
+pie = (3 + 4) * [n]
 
 main = do
   let x = pie !! 100
