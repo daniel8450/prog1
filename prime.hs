1d55d03f786eee6d2d8f5a78919b19e1706f5c3a
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 07:27:11 -0600

    Update prime.hs

--- prime.hs
+++ prime.hs
@@ -2,7 +2,7 @@
 --isPrime n 0 = False
 --isPrime n 1 = False
 --isPrime n m = if ((mod n m) /= 0) then True
-              else (isPrime n (m-1))
+             -- else (isPrime n (m-1))
       
 --prime : Int -> [Int]
 --prime n | (isPrime
