22194e5c52b8409d4132b9da85b4309d3e90614b
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-05 03:10:09 -0600

    Update pi.hs

--- pi.hs
+++ pi.hs
@@ -5,24 +5,24 @@
 --n = [-1**k / m !! 100 | k <- [0..100]]
 
 
---n :: [Float]
---n = [((-1)**k) / ((2*k+2)*(2*k+3)*(2*k+4)) | k <- [0..100]]
+n :: [Float]
+n = [((-1)**k) / ((2*k+2)*(2*k+3)*(2*k+4)) | k <- [0..100] where k + 2]
 
 
---pie :: Float
---pie = (3 + 4) * n !! 100
+pie :: Float
+pie = (3 + 4) * n !! 100
 -------------------------------------------------------------
-three :: Float
-three = 3
+---three :: Float
+--three = 3
 
-add :: Float -> Float
-add x = x + three
+--add :: Float -> Float
+--add x = x + three
 
-pie :: Float -> Float
-pie n = (add 4) / (n*(n+1)*(n+2))
+--pie :: Float -> Float
+--pie n = (add 4) / (n*(n+1)*(n+2))
 
-getPie :: Float
-getPie = pie (n+2)
+--getPie :: Float
+--getPie = pie (n+2)
 
 main = do
   let x = pie
