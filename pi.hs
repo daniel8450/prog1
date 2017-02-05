0fab84fc52fa09da794612fec02b2185f154df4f
Daniel LaBorde <dlabo14@lsu.edu>
2017-02-04 23:47:11 -0600

    Update pi.hs

--- pi.hs
+++ pi.hs
@@ -1,14 +1,34 @@
-m :: [Float]
-m = [(2*k+2)*(2*k+3)*(2*k+4) | k <- [0..100]]
+--m :: [Float]
+--m = [(2*k+2)*(2*k+3)*(2*k+4) | k <- [0..100]]
 
-n :: [Float]
-n = [-1**k / m !! 100 | k <- [0..100]]
+--n :: [Float]
+--n = [-1**k / m !! 100 | k <- [0..100]]
+
+
 --n :: [Float]
 --n = [-1**k / (2*k+2)*(2*k+3)*(2*k+4) | k <- [0..100]]
 
 
-pie :: Float
-pie = (3 + 4) * n !! 100
+--pie :: Float
+--pie = (3 + 4) * n !! 100
+
+three :: Float
+three = 3
+
+add :: Float -> Float
+add x = x + three
+
+pie :: [Float]
+pie = [add 4 / (n*(n+1)*(n+2)) | n <- [0..100] n + 2]
+
+
+
+
+
+
+
+
+
 
 
 main = do
